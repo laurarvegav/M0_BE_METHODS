39725947ba7004651829778e200ad3db0bd6e9b3
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-11 11:38:15 -0700

    Remove comment on the code lines for the whole document

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -9,30 +9,30 @@
 "Hello World".downcase
 
 
-"Hello World".include?("Hello")
+puts "Hello World".include?("Hello")
 # The include? method is called on the string "Hello World" with the parameter "Hello"
 # I expected it to print True or False, given that the method ends with '?' No arguments passed
 # The method evaluates if it is true or false that the parameter is included on the string object
 # The return value was True
 
 
-"Hello World".end_with?("Hello")
+puts "Hello World".end_with?("Hello")
 # The end.with? method is called on the object string "Hello World" with the parameter "Hello"
 # No arguments passed, end_with evaluates if it is true or false that the parameter is the last 
 # word of the string object. The return value was False
 
-"Hello World".end_with?("rld")
+puts "Hello World".end_with?("rld")
 # The end.with? method is called on the object string "Hello World" with the parameter "rld"
 # No arguments passed, end_with evaluates if it is true or false that the parameter is  in the
 # last word of the string object. The return value was True
 
-12.even?
+puts 12.even?
 # The even? method is called on the integer 12, I could predict it evaulates if the parameter
 # is even, and would show result True
 # My prediction was right, the even? method evaluates if is true or false that the parameter
 # is even
 
-18.next
+puts 18.next
 # The even? method is called on the parameter integer 18, I can predict it will give us
 # the integer that goes next to the parameter, in this case 19
 # My prediction was right, the next method prints the next integer to the parameter. The
@@ -54,16 +54,16 @@
 # first_name = "Jeff"
 # puts first_name.start_with?("J")
 
-# school_teachers_name = "Stacy"
-# swimming_teachers_name = "Max"
+school_teachers_name = "Stacy"
+swimming_teachers_name = "Max"
 
-# puts school_teachers_name.size
+puts school_teachers_name.size
 # # The size method is called on the school_teachers_name variable, which stores the string "Stacy"
 # # The return value of the method size is the number of characters contained in the called value
 # # In this example, the return value is 5, because "stacy" is a string of 5 characters
 # # The puts command prints the return value of size (5) to the console.
 
-# puts swimming_teachers_name.match?(school_teachers_name)
+puts swimming_teachers_name.match?(school_teachers_name)
 # # The match? method is called on the swimming_teachers_name variable, which stores the string
 # # "Max". The match? method returns true if a match is found for the data in the 
 # # swimming_teachers_name and the data in the pattern school_teachers_name, which stores the
@@ -78,16 +78,16 @@
 # # https://ruby-doc.org/core-3.1.0/Integer.html
 # # Include comments above each method call explaining the impact and return value of that method.
 
-# total_number_residents = 36
-# floor_number = 5
+total_number_residents = 36
+floor_number = 5
 
-# puts total_number_residents.even?
+puts total_number_residents.even?
 # # The even? method is called on the total_number_residents variable, which stores the integer 36
 # # The return value of the method even? is true if the data stored in total_number_residents is even
 # # In this example, the return value is true, because 36 is an even integer.
 # # The puts command returns the value of even? method (true) to the console.
 
-# puts floor_number.succ
+puts floor_number.succ
 # The succ method is called on the floor_number variable, which stores the integer 5
 # The return value of the method succ is the successor integer of the number contained 
 # in the called variable, equivalent to adding a unit to the called variable.
