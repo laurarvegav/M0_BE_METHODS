d77e7d5fed2d61e28720517cc4a9188d644fa289
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-11 10:52:53 -0700

    Comment out previous sections

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -51,43 +51,43 @@
 # The start_with? method returns true if the data in the first_name variable starts with the argument passed in.
 # In this example, the return value is true, because "Jeff" does start with "J".
 # The puts command prints the return value of the start_with? method (true) to the console.
-first_name = "Jeff"
-puts first_name.start_with?("J")
+# first_name = "Jeff"
+# puts first_name.start_with?("J")
 
-school_teachers_name = "Stacy"
-swimming_teachers_name = "Max"
+# school_teachers_name = "Stacy"
+# swimming_teachers_name = "Max"
 
-puts school_teachers_name.size
-# The size method is called on the school_teachers_name variable, which stores the string "Stacy"
-# The return value of the method size is the number of characters contained in the called value
-# In this example, the return value is 5, because "stacy" is a string of 5 characters
-# The puts command prints the return value of size (5) to the console.
+# puts school_teachers_name.size
+# # The size method is called on the school_teachers_name variable, which stores the string "Stacy"
+# # The return value of the method size is the number of characters contained in the called value
+# # In this example, the return value is 5, because "stacy" is a string of 5 characters
+# # The puts command prints the return value of size (5) to the console.
 
-puts swimming_teachers_name.match?(school_teachers_name)
-# The match? method is called on the swimming_teachers_name variable, which stores the string
-# "Max". The match? method returns true if a match is found for the data in the 
-# swimming_teachers_name and the data in the pattern school_teachers_name, which stores the
-# string "Stacy". In this example, the return value is false, because the data contained in 
-# swimming_teachers_name does not mactch the data contained in school_teachers_name.
-# The puts command prints the return value of match? method (false) to the console.
+# puts swimming_teachers_name.match?(school_teachers_name)
+# # The match? method is called on the swimming_teachers_name variable, which stores the string
+# # "Max". The match? method returns true if a match is found for the data in the 
+# # swimming_teachers_name and the data in the pattern school_teachers_name, which stores the
+# # string "Stacy". In this example, the return value is false, because the data contained in 
+# # swimming_teachers_name does not mactch the data contained in school_teachers_name.
+# # The puts command prints the return value of match? method (false) to the console.
 
 
-# SECTION 3: Calling methods on variables assigned to integers.
-# Declare 2 variables assigned to integer objects.
-# Call a different built-in Ruby method on each of your variables. 
-# https://ruby-doc.org/core-3.1.0/Integer.html
-# Include comments above each method call explaining the impact and return value of that method.
+# # SECTION 3: Calling methods on variables assigned to integers.
+# # Declare 2 variables assigned to integer objects.
+# # Call a different built-in Ruby method on each of your variables. 
+# # https://ruby-doc.org/core-3.1.0/Integer.html
+# # Include comments above each method call explaining the impact and return value of that method.
 
-total_number_residents = 36
-floor_number = 5
+# total_number_residents = 36
+# floor_number = 5
 
-puts total_number_residents.even?
-# The even? method is called on the total_number_residents variable, which stores the integer 36
-# The return value of the method even? is true if the data stored in total_number_residents is even
-# In this example, the return value is true, because 36 is an even integer.
-# The puts command returns the value of even? method (true) to the console.
+# puts total_number_residents.even?
+# # The even? method is called on the total_number_residents variable, which stores the integer 36
+# # The return value of the method even? is true if the data stored in total_number_residents is even
+# # In this example, the return value is true, because 36 is an even integer.
+# # The puts command returns the value of even? method (true) to the console.
 
-puts floor_number.succ
+# puts floor_number.succ
 # The succ method is called on the floor_number variable, which stores the integer 5
 # The return value of the method succ is the successor integer of the number contained 
 # in the called variable, equivalent to adding a unit to the called variable.
@@ -99,3 +99,5 @@ puts floor_number.succ
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+
